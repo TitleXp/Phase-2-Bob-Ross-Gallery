61dc706225d4e111c1cfb6ea9e7d8df4624897c0
Surapat Mekvanich <titleyugio@gmail.com>
2022-12-27 15:20:19 -0800

    added onclick to the pictures

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,17 +1,45 @@
+import { useState } from "react";
+
 function Painting ({ painting }) {
+
+    const [showStats, setShowStats] = useState(true)
+
+    const handleClick = () => {
+        setShowStats(currentValue => !currentValue)
+    }
+
+    // const paintingColors = painting.colors.toString().toString()
+
     return (
 <li className="cards__item">
-      <div className="card">
-        <img
-          src={painting.img_src}
-          alt={painting.painting_title}
-          className="card__image"
-        />
-        <div className="card__content">
-          <div className="card__title">{painting.painting_title}</div>
-          <p className="card__text">Card Text</p>
-          <div className="card__detail"></div> 
-    </div>
+    <div className="card" onClick={handleClick}>
+            
+        <div className="card__title">{painting.painting_title}</div>
+
+            <a href={painting.img_src} ></a>
+            {showStats ? (<img className="card__image" src={painting.img_src} alt={painting.painting_title} />) : (
+                <>
+            <span className="card__detail">Number of Colors: {painting.num_colors}</span>
+            <span className="card__detail">Colors: {painting.colors} </span>       
+                </>
+            )}
+
+
+            {/* <img onClick={handleClick} 
+            src={painting.img_src}
+            alt={painting.painting_title}
+            className="card__image" 
+            />
+
+            <a className="card__detail">Number of Colors: {painting.num_colors}</a>
+            <a className="card__detail">Colors: {painting.colors} </a> */}
+
+            <div className="card__content">
+                <a href={painting.youtube_src}>
+                    Season: {painting.season} Episode:{painting.episode}
+                </a>
+                <div className="card__detail"></div> 
+            </div>
     </div>
     
     
