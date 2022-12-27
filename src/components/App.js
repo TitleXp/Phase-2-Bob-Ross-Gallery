fc03cad62aad8b8e7268d9f0b31caff44d4ea0de
Dylan Curtis <Dcurtisj@gmail.com>
2022-12-27 11:56:06 -0800

    add PaintingsContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,49 @@
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import '../App.css';
+import { useState, useEffect } from 'react';
+import Header from "./Header";
+import PaintingsContainer from "./PaintingsContainer";
 
 function App() {
+ const [paintings, setPaintings]=useState([])
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const resp = await fetch("http://localhost:3000/paintings")
+      const paintings = await resp.json()
+      setPaintings(paintings)
+    } catch (error) {
+      alert(error)
+    }
+   }
+
+   fetchData()
+  
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Header/>
+   <PaintingsContainer/>
     </div>
   );
 }
 
 export default App;
+
+
+
+{/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header> */}
